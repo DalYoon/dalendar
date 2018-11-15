321952385eb6af75ee6b02a19165ad2552d7cd4f
Dal Yoon <yeodal89@gmail.com>
2018-11-15 14:42:28 +0900

    Updates

---
+++ precache-manifest.583f6d0477b261426a3bc05a00f47053.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "92f82c985e2d2549978c",
+    "url": "/dalendar/static/js/runtime~main.92f82c98.js"
+  },
+  {
+    "revision": "b0e7eb12532dab0d8d67",
+    "url": "/dalendar/static/js/main.b0e7eb12.chunk.js"
+  },
+  {
+    "revision": "20313b4ca8be93344cd7",
+    "url": "/dalendar/static/js/1.20313b4c.chunk.js"
+  },
+  {
+    "revision": "9ab89d7471e878537cacd20cb395a7ec",
+    "url": "/dalendar/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/dalendar/precache-manifest.583f6d0477b261426a3bc05a00f47053.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/dalendar/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
